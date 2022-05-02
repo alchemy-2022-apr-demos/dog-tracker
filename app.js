5d9583c69811d82f78c8af97870b55f0aafb8358
Julie Nisbet <jmnisbet@gmail.com>
2022-05-02 10:14:35 -0700

    FormData and listing dogs

--- app.js
+++ app.js
@@ -1,8 +1,46 @@
-// import functions and grab DOM elements
+const dogForm = document.getElementById('dog-form');
+const dogList = document.getElementById('dog-list');
 
-// let state
+let dogs = [];
+dogForm.addEventListener('submit', (e) => {
+    // what is e??
+    // e is what gets passed to your callback
+    // function by the browswer
+    // contains a whole bunch of info
+    // about the event
+    // e is a booger or banana -- i.e. its just a variable name
+    // generally we call it e or event
+    // console.log(e);
+    e.preventDefault();
+    // console.log('submitting the form');
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+    const dogData = new FormData(dogForm);
+    // console.log(dogData.get('dog-name'));
+    // console.log(dogData.get('age'));
+
+    let dog = {
+        name: dogData.get('dog-name'),
+        age: dogData.get('age'),
+    };
+
+    dogs.push(dog);
+    console.log('list of dogs', dogs);
+
+    renderDogs();
+
+    // resets all the inputs in the form
+    dogForm.reset();
+});
+
+function renderDogs() {
+    // clear out the existing list
+    dogList.textContent = '';
+    // loop through each dog in dogs
+    for (let dog of dogs) {
+        //   create an li with the dog's name and age
+        const dogLI = document.createElement('li');
+        dogLI.textContent = `${dog.name}: ${dog.age} years`;
+        //   append the li to the ul
+        dogList.append(dogLI);
+    }
+}
